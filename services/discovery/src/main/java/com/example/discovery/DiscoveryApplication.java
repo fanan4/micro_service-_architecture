ec029e4d4bb2211dc61731bbeab7c16b49fafa67
abdelouahedGit <abdeouahedfanan@gmail.com>
2024-10-24 17:46:54 +0200

    add the discovery server

--- services/discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
+++ services/discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.discovery.discovery;
+package com.example.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
 
 }
